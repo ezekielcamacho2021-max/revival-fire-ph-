16fcc5f04a6a15d253b09dd924a8ba7d450e818b
ezekielcamacho2021-max <ezekielcamacho2021@gmail.com>
2026-06-02 09:29:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-// Revival Fire Philippines JavaScript
+const hamburger=document.querySelector('.hamburger');
+const nav=document.querySelector('.nav-links');
+
+if(hamburger){
+hamburger.addEventListener('click',()=>{
+nav.classList.toggle('active');
+});
+}
